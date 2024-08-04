d7b421a198dbb90c120f4b9103ded72a8994697f
Samruddhi Shinde <114212065+samruddhishinde17@users.noreply.github.com>
2024-08-04 18:45:01 +0530

    Add files via upload

---
+++ program111.c
@@ -0,0 +1,55 @@
+/*
+    iRow = 4
+    iCol = 4
+
+
+    *   *   *   *
+    *           *
+    *           *
+    *   *   *   *
+
+*/
+
+#include<stdio.h>
+
+void Display(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    if(iRow != iCol)        // Filter
+    {
+        printf("Number of rows and number of columns should be same\n");
+        return;
+    }
+
+    for (i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            if ((i == 1) || (j == iCol) || (i == iRow) || (j == 1))
+            {
+                printf("*\t");
+            }
+            else
+            {
+                printf(" \t");
+            }
+        }
+        printf("\n"); 
+    } 
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter the no of rows = \n");
+    scanf("%d",&iValue1);
+
+    printf("Enter the no of columns = \n");
+    scanf("%d",&iValue2);
+
+    Display(iValue1, iValue2);
+
+    return 0;
+}
